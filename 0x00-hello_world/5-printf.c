fc70a3cf7f959f5e2231d0a80fdc159867f2f86b
root <peacehonest608@gmail.com>
2023-02-20 07:24:32 -0800

    s

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
-/**
-*You should produce the exact same output as in the example
-*ou might have to install the package libc6-dev-i386 on your Linux to test the -m32 gcc option
-*/
-
+	/**
+	 *You should produce the exact same output as in the example
+	 *ou might have to install the package libc6-dev-i386 on your Linux to test the -m32 gcc option
+	 */
 int main(void)
 {
 	/**
-	* I want this 
-	*to work
-	*/
+	 * I want this 
+	 *to work
+	 */
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return 0;
 }
