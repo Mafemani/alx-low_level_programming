3f16972cc348f4b70dd0ea5311dd095a25ba6677
root <peacehonest608@gmail.com>
2023-02-20 07:28:19 -0800

    s

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
-	/**
-	 *You should produce the exact same output as in the example
-	 *ou might have to install the package libc6-dev-i386 on your Linux to test the -m32 gcc option
-	 */
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	/**
-	 * I want this 
-	 *to work
+	 * puts - print a string to the console followed by a newline character
+	 *
+	 * Description: This function takes a string as input and prints it to
+	 * the console, followed by a newline character.
+	 *
+	 * Return: On success, returns a non-negative integer.
+	 * On error, returns EOF.
 	 */
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return 0;
