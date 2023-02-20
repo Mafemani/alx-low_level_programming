f49b868e75d4b1f4b1effeb0432912a984ef9385
root <peacehonest608@gmail.com>
2023-02-20 06:42:56 -0800

    k

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
-int main(void) {
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/**
+	 * puts - print a string to the console followed by a newline character
+	 *
+	 * Description: This function takes a string as input and prints it to
+	 * the console, followed by a newline character.
+	 *
+	 * Return: On success, returns a non-negative integer.
+	 * On error, returns EOF.
+	 */
     puts("Programming is like building a multilingual puzzle");
-    return 0;
+
+    return (0);
 }
