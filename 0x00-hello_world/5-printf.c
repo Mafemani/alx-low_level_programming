9a8f7ef564bf10af37f20a026ee3c010447097bf
root <peacehonest608@gmail.com>
2023-02-20 07:21:12 -0800

    a

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-/*
+/**
 *You should produce the exact same output as in the example
 *ou might have to install the package libc6-dev-i386 on your Linux to test the -m32 gcc option
 */
 
 int main(void)
 {
-	/* I want this 
+	/**
+	* I want this 
 	*to work
 	*/
 	printf("with proper grammar, but the outcome is a piece of art,\n");
