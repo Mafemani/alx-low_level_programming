cf4a28e2c01ef0b1af3d1c6c39af6d56b0240a71
root <peacehonest608@gmail.com>
2023-02-21 10:27:28 -0800

    a

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+* print_alphabet - prints the lowercase alphabet followed by a new line
+*/
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+}
