c9b070b439652807a95346adc7bb904324a802bc
root <peacehonest608@gmail.com>
2023-02-21 10:04:26 -0800

    s

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,30 +1,30 @@
 #include <unistd.h>
 
 /**
- * main - entry point of the program
- *
- * Return: Always 0 (Success)
- */
+* main - entry point of the program
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-    char c = '_';
+	char c = '_';
 
-    write(STDOUT_FILENO, &c, 1);
-    c = 'p';
-    write(STDOUT_FILENO, &c, 1);
-    c = 'u';
-    write(STDOUT_FILENO, &c, 1);
-    c = 't';
-    write(STDOUT_FILENO, &c, 1);
-    c = 'c';
-    write(STDOUT_FILENO, &c, 1);
-    c = 'h';
-    write(STDOUT_FILENO, &c, 1);
-    c = 'a';
-    write(STDOUT_FILENO, &c, 1);
-    c = 'r';
-    write(STDOUT_FILENO, &c, 1);
-    c = '\n';
+	write(STDOUT_FILENO, &c, 1);
+	c = 'p';
+	write(STDOUT_FILENO, &c, 1);
+	c = 'u';
+	write(STDOUT_FILENO, &c, 1);
+	c = 't';
+	write(STDOUT_FILENO, &c, 1);
+	c = 'c';
+	write(STDOUT_FILENO, &c, 1);
+	c = 'h';
+	write(STDOUT_FILENO, &c, 1);
+	c = 'a';
+	write(STDOUT_FILENO, &c, 1);
+	c = 'r';
+	write(STDOUT_FILENO, &c, 1);
+	c = '\n';
  	write(STDOUT_FILENO, &c, 1);
 
 	return (0);
